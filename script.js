24f3749bd2e5876bc5f7ebabdf9ebd280f85f908
arcenunez12 <andrea.arce.nunez@gmail.com>
2023-08-19 22:56:44 -0400

    Created HTML, CSS, and Javascript files

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    // Use Math.random() to get a random number b/t 0 and 2
+    let randomNum = Math.random;
+    console.log(randomNum);
+}
+getComputerChoice();
+console.log("Hello World!");
