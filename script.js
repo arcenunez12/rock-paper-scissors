f7d2e08e16635776e77980a20f72c6dd4bc8c5d3
arcenunez12 <andrea.arce.nunez@gmail.com>
2023-08-20 00:20:02 -0400

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function getComputerChoice() {
     // Use Math.random() to get a random number b/t 0 and 2
-    let randomNum = Math.random;
+    let randomNum = Math.random() * 3;
+    randomNum = Math.ceil(randomNum);
     console.log(randomNum);
 }
 getComputerChoice();
